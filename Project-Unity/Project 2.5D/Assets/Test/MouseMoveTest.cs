d1fbc6b249cba1b1dfa05347f310438e36c8201c
Vitaliy <51445834+OrangeMGE@users.noreply.github.com>
2019-06-19 17:23:53 +0300

    Тест-Билды

---
+++ Project-Unity/Project 2.5D/Assets/Test/MouseMoveTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMoveTest : MonoBehaviour
+{
+    public GameObject go;
+    public float sensitivity = 1F;
+    private Camera goCamera;
+    private Vector3 MousePos;
+    private float MyAngle = 0F;
+
+    void Start()
+    {
+        // Инициализируем кординаты мышки и ищем главную камеру
+        goCamera = Camera.main;
+        go = goCamera.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        MousePos = Input.mousePosition;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            MyAngle = 0;
+
+            MyAngle = sensitivity * ((MousePos.x - (Screen.width / 2)) / Screen.width);
+            goCamera.transform.RotateAround(go.transform.position, goCamera.transform.up, MyAngle);
+            MyAngle = sensitivity * ((MousePos.y - (Screen.height / 2)) / Screen.height);
+            goCamera.transform.RotateAround(go.transform.position, goCamera.transform.right, -MyAngle);
+        }
+    }
+}
---
+++ Project-Unity/Project 2.5D/Assets/Test/TestMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    public Text UIText;
+
+    public Transform startTransformToCamera;
+
+    void FixedUpdate() {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        UIText.text = $"Horizontal:{horizontalAxis} \n" +
+                      $"Vertical:{verticalAxis} \n\n" +
+                      $"X:{gameObject.transform.position.x}\n" +
+                      $"Y:{gameObject.transform.position.y}\n" +
+                      $"Z:{gameObject.transform.position.z}\n" +
+                      $"MousePosition:{new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.z)}\n";
+
+        gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + verticalAxis * speed);
+        gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x + horizontalAxis * speed, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    /*  void Update() {
+        RaycastHit hit;
+        Debug.DrawRay(startTransformToCamera.position, Input.mousePosition, Color.red);
+        Ray ray = new Ray(startTransformToCamera.position, new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.z));
+    }   */
+}
